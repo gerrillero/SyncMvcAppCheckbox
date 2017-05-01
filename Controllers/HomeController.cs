1bbb7d3116831942f967617d5f2b5e97188bd9fa
Antonio <antonio75@gmail.com>
2017-05-01 22:21:02 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿#region Copyright Syncfusion Inc. 2001 - 2017
+// Copyright Syncfusion Inc. 2001 - 2017. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using SyncMvcAppCheckbox.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SyncMvcAppCheckbox.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TestViewModel model = new TestViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(TestViewModel model)
+        {
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/TestViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SyncMvcAppCheckbox.Models
+{
+    public class TestViewModel
+    {
+        public Int32 Id { get; set; }
+        public Boolean IsChecked1 { get; set; }
+        public Boolean IsChecked2 { get; set; }
+        public Boolean IsChecked3 { get; set; }
+
+        public TestViewModel()
+        {
+            IsChecked1 = true;
+            IsChecked2 = true;
+            IsChecked3 = true;
+        }
+    }
+}
